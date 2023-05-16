5ddcffacab6531c35a29886dfdaeb4b6d7eeed65
Nikita Kotov <kotovnikita00744@gmail.com>
2023-05-16 12:18:14 +0700

    dock: Добавлено описание контрактов аутентификации, подписки на пользователя, получение подписчиков

--- backend/src/main/java/ru/axnikita/praktika/model/Post.java
+++ backend/src/main/java/ru/axnikita/praktika/model/Post.java
@@ -2,5 +2,5 @@ package ru.axnikita.praktika.model;
 
 import java.util.List;
 
-public record Post(String title, String description, String text, boolean isPublic, List<String> tags) {
+public record Post(String login, String id, String title, String description, String text, boolean isPublic, List<Tag> tags) {
 }
---
+++ backend/src/main/java/ru/axnikita/praktika/model/Tag.java
@@ -0,0 +1,5 @@
+package ru.axnikita.praktika.model;
+
+public enum Tag {
+    FRONTEND, BACKEND, JAVASCRIPT, JAVA, SECURITY, ANALYTICS
+}

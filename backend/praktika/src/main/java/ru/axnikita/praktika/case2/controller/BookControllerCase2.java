f385448380aeb85e52f43088e9d6f3fa7f25a82b
Nikita Kotov <kotovnikita00744@gmail.com>
2023-05-28 14:39:06 +0700

    CASE-2 feature: эндпоинты

--- backend/praktika/src/main/java/ru/axnikita/praktika/case2/controller/BookControllerCase2.java
+++ backend/praktika/src/main/java/ru/axnikita/praktika/case2/controller/BookControllerCase2.java
@@ -4,10 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.axnikita.praktika.case2.entity.BookAvailabilityCase2;
 import ru.axnikita.praktika.case2.entity.BookEntityCase2;
-import ru.axnikita.praktika.case2.entity.BookStatusCase2;
+import ru.axnikita.praktika.case2.model.BookCategory;
 import ru.axnikita.praktika.case2.repository.BookRepositoryCase2;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/case2")
@@ -22,22 +23,31 @@ public class BookControllerCase2 {
     @GetMapping("/db-init")
     public ResponseEntity<String> initBookDB() {
         try {
-            bookRepository.save(BookEntityCase2.defaultBookOf("Java Book", "Some description", "action", "Nik", "10.02.2001", "200 руб.", "100 руб."));
-            bookRepository.save(BookEntityCase2.defaultBookOf("Javascript Book", "Some description", "action", "Nik", "10.02.2001", "300 руб.", "200 руб."));
-            bookRepository.save(BookEntityCase2.defaultBookOf("Pascal Book", "Some description", "action", "Nik", "10.02.2001", "400 руб.", "300 руб."));
+            bookRepository.save(BookEntityCase2.defaultBookOf("Java Book", "Some description", List.of(BookCategory.documentation, BookCategory.balalar), "Nik", "10.02.2001", "200 руб.", "100 руб."));
+            bookRepository.save(BookEntityCase2.defaultBookOf("Javascript Book", "Some description", List.of(BookCategory.classic, BookCategory.fantasy), "Nik", "10.02.2001", "300 руб.", "200 руб."));
+            bookRepository.save(BookEntityCase2.defaultBookOf("Pascal Book", "Some description", List.of(BookCategory.documentation, BookCategory.balalar), "Nik", "10.02.2001", "400 руб.", "300 руб."));
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/books")
-    public ResponseEntity<List<BookEntityCase2>> getAllBook() {
-        return ResponseEntity.ok(bookRepository.findAll());
+
+    @GetMapping("/book")
+    public ResponseEntity<BookEntityCase2> getBookById(@RequestParam long id) {
+        try {
+            Optional<BookEntityCase2> found = bookRepository.findById(id);
+            if (found.isEmpty()) {
+                return ResponseEntity.internalServerError().build();
+            }
+            return ResponseEntity.ok(found.get());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @PostMapping("/book")
-    public ResponseEntity<String> creteOrUpdateBook(BookEntityCase2 bookEntity) {
+    public ResponseEntity<BookEntityCase2> addBook(@RequestBody BookEntityCase2 bookEntity) {
         try {
             bookRepository.save(bookEntity);
         } catch (Exception e) {
@@ -46,31 +56,87 @@ public class BookControllerCase2 {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/book")
-    public ResponseEntity<String> deleteBook(BookEntityCase2 bookEntity) {
+    @PutMapping("/book")
+    public ResponseEntity<BookEntityCase2> editBookById(@RequestParam long id,
+                                                        @RequestBody BookEntityCase2 bookEntity) {
         try {
-            bookRepository.deleteById(bookEntity.getId());
+            Optional<BookEntityCase2> found = bookRepository.findById(id);
+            found.ifPresent(entity -> bookRepository.save(entity.baseOnAddId(bookEntity, entity.getId())));
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/user-books")
-    public ResponseEntity<List<BookEntityCase2>> getAllBook(@RequestParam(required = false) String login) {
+    @DeleteMapping("/book")
+    public ResponseEntity<String> deleteBookById(@RequestParam long id) {
+        try {
 
-        List<BookEntityCase2> books;
+            bookRepository.deleteById(id);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().build();
+    }
 
-        if (!login.isBlank()) {
-            books = bookRepository.findBooksByLogin(login);
-        } else {
-            books = bookRepository.findAll().stream()
-                    .filter(book -> book.getStatus().equals(BookStatusCase2.PUBLIC))
-                    .filter(book -> book.getAvailability().equals(BookAvailabilityCase2.AVAILABLE))
-                    .toList();
+    @GetMapping("/books")
+    public ResponseEntity<List<BookEntityCase2>> getAllBook(@RequestParam(required = false) String author,
+                                                            @RequestParam(required = false) BookCategory category,
+                                                            @RequestParam(required = false) String year) {
+
+        List<BookEntityCase2> allBooks = bookRepository.findAll();
+
+        if (author != null) {
+            return ResponseEntity.ok(allBooks.stream().filter(entity -> entity.getAuthor().equals(author)).toList());
         }
 
-        return ResponseEntity.ok(books);
+        if (category != null) {
+            return ResponseEntity.ok(allBooks.stream().filter(entity -> entity.getCategory().contains(category)).toList());
+        }
+
+        if (year != null) {
+            return ResponseEntity.ok(allBooks.stream().filter(entity -> entity.getDate().contains(year)).toList());
+        }
+
+        return ResponseEntity.ok(allBooks);
+    }
+
+    @PostMapping("/buy")
+    public ResponseEntity<String> buyBook(@RequestParam String login,
+                                          @RequestParam long bookId) {
+        try {
+
+            Optional<BookEntityCase2> found = bookRepository.findById(bookId);
+            found.ifPresent(entity -> {
+                entity.setLogin(login);
+                entity.setAvailability(BookAvailabilityCase2.BUY);
+                bookRepository.save(entity);
+            });
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/rent")
+    public ResponseEntity<String> rentBook(@RequestParam String login,
+                                           @RequestParam long bookId,
+                                           @RequestParam String reservedUntil) {
+        try {
+
+            Optional<BookEntityCase2> found = bookRepository.findById(bookId);
+            found.ifPresent(entity -> {
+                entity.setLogin(login);
+                entity.setAvailability(BookAvailabilityCase2.RENT);
+                entity.setReservedUntil(reservedUntil);
+                bookRepository.save(entity);
+            });
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().build();
     }
 
 }
--- backend/praktika/src/main/java/ru/axnikita/praktika/model/Auth.java
+++ backend/praktika/src/main/java/ru/axnikita/praktika/model/Auth.java
@@ -1,6 +1,4 @@
 package ru.axnikita.praktika.model;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record Auth(String login, String password) {
 }
